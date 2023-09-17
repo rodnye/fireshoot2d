dc9b4791d0f8b2efbf84423a0b0d401dfc384d87
Rodny Estrada <rrodnyestrada1@gmail.com>
2023-09-17 13:44:06 -0400

    feat (serv): added pages routes

--- serv/server.js
+++ serv/server.js
@@ -78,13 +78,33 @@ app.listen(config.PORT, () => {
 // Serve static pages
 //
 if (!config.isProduction) {
+
     // Development mode - Use Webpack server for serving static files
     const webpackRouter = require('./webpack-router');
+    const webpackCompiler = webpackRouter.compiler;
+    
     app.use("/", webpackRouter);
+
+    for (let page of config.pages) {
+        app.get(`/${page}`, (req, res) => { 
+            webpackCompiler.outputFileSystem.readFile(`./dist/${page}.html`, (err, result) => {
+                if (err) console.log(err);
+                res.set("content-type", "text/html");
+                res.send(result); 
+            })
+        });
+    }
+
     console.log("Using Webpack server for development mode...");
+
 }
 else {
     // Production mode - Use static server for serving static files
-    app.use("/", express.static(config.DIST));
+    app.use("/public", express.static(config.DIST + "/public"));
+
+    for (let page of config.pages) {
+        app.get(`/${page}`, (req, res) => res.sendFile(path.join(config.DIST, page + ".html")))
+    }
+
     console.log("Using static server for production mode...");
 }
