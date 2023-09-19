376f22f6217187a01b584861d394ea0aaff24c43
TheNewestNewbie <142923286+TheNewestNewbie@users.noreply.github.com>
2023-09-19 13:48:18 -0500

    fix (serv/socket): fixed check of map data retrieved

--- serv/engine/socket.js
+++ serv/engine/socket.js
@@ -9,7 +9,7 @@ const Maps = require("./map.js");
 
 module.exports = async (io) => {
     const g = G(io);
-    
+
     let maps = new Maps();
     maps.load(config.DB + "/maps");
     
@@ -17,23 +17,27 @@ module.exports = async (io) => {
 
     g.on("connection" , async (socket) => { 
         const s = S(socket);
+        //checking if user is on session storage , if not reject
         if(!s.request.session || !s.request.session.passport || !s.request.session.passport.user) return s.disconnect();
-        const user_id = s.request.session.passport.user;
+        const user_id = /*"pkZI01f3" ||*/  s.request.session.passport.user;
         console.log("Your User ID is", user_id);
         spos.name = "pj_" + uid.num(5);
         spos.user_id = user_id;
+        //creating player if not exist in db and loading if exists
         const [_pj, created] = await Pj.findOrCreate({
             where: { user_id: user_id },
             defaults: spos
           });
         let pj = _pj || created;
-
+        
+        //retrieving map data if user havent , returning true if user have the map updated
         s.on("get_map" , (data) => {
-            if(data.vhash && data.vhash.isArray() && maps.get(pj.m) && data.vhash.includes(maps.get(pj.m).vhash)) {
+            if(data.vhash && Array.isArray(data.vhash) && maps.get(pj.m) && data.vhash.includes(maps.get(pj.m).vhash)) {
                 s.emit("get_map" , true);
             } else s.emit("get_map" , maps.get(pj.m));
         });
 
+        //retrieving players data
         let player = new Player(pj.user_id , pj.name , s , pj.x , pj.y , pj.a , pj.m);
         s.on("get_players" , (data) => {
             players.add(player);
@@ -45,6 +49,6 @@ module.exports = async (io) => {
     //Game Loop
 	setInterval(function(){
         let pom = players.getPosByMap();
-		for(let m in pom) g.to(m).emit('pj_pos', pom[m]);
+		for(let m in pom) g.to(m).emit('pj_pos', pom[m]); //sending player position to every room
 	}, 30);
 }
