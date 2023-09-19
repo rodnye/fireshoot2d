6abb42f2ab2799579d11d53412f9752fb94053d7
Rodny Estrada <rrodnyestrada1@gmail.com>
2023-09-19 01:24:43 -0400

    feat (pages/auth): added AuthButton components to view

--- src/pages/auth/App.jsx
+++ src/pages/auth/App.jsx
@@ -1,10 +1,11 @@
 
+import AuthButton from "ui/AuthButton"
 
 export default function App () {
     return (
-        <div className="w-100 d-flex flex-column justify-content-center align-items-center">
-            <div> Google </div>
-            <div> Facebook </div>
+        <div className="w-100 vh-100 d-flex flex-column justify-content-center align-items-center">
+            <AuthButton type="google"/>
+            <AuthButton type="facebook"/>
         </div>
     )
 }
